151c7b4e0f6dddef730413534584dc3fc2110265
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-02-04 20:38:57 -0600

    Fix Sawmill recipe type

--- src/main/java/com/teamabnormals/woodworks/core/Woodworks.java
+++ src/main/java/com/teamabnormals/woodworks/core/Woodworks.java
@@ -1,6 +1,5 @@
 package com.teamabnormals.woodworks.core;
 
-import com.google.common.collect.ImmutableList;
 import com.teamabnormals.blueprint.core.util.registry.RegistryHelper;
 import com.teamabnormals.woodworks.core.data.client.WoodworksBlockStateProvider;
 import com.teamabnormals.woodworks.core.data.client.WoodworksLanguageProvider;
@@ -13,19 +12,14 @@ import com.teamabnormals.woodworks.core.other.WoodworksClientCompat;
 import com.teamabnormals.woodworks.core.other.WoodworksCompat;
 import com.teamabnormals.woodworks.core.registry.WoodworksLootConditions;
 import com.teamabnormals.woodworks.core.registry.WoodworksMenuTypes;
-import com.teamabnormals.woodworks.core.registry.WoodworksRecipes;
 import com.teamabnormals.woodworks.core.registry.WoodworksRecipes.WoodworksRecipeSerializers;
 import com.teamabnormals.woodworks.core.registry.WoodworksRecipes.WoodworksRecipeTypes;
 import com.teamabnormals.woodworks.core.registry.helper.WoodworksBlockSubRegistryHelper;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.world.inventory.RecipeBookType;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RegisterRecipeBookCategoriesEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -38,8 +32,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class Woodworks {
 	public static final String MOD_ID = "woodworks";
 	public static final RegistryHelper REGISTRY_HELPER = RegistryHelper.create(MOD_ID, helper -> helper.putSubHelper(ForgeRegistries.BLOCKS, new WoodworksBlockSubRegistryHelper(helper)));
-
-	public static final RecipeBookType RECIPE_TYPE_SAWING = RecipeBookType.create("SAWING");
 
 	public Woodworks() {
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -55,10 +47,6 @@ public class Woodworks {
 		bus.addListener(this::commonSetup);
 		bus.addListener(this::clientSetup);
 		bus.addListener(this::dataSetup);
-
-		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
-			bus.addListener(this::registerCategories);
-		});
 
 		context.registerConfig(ModConfig.Type.COMMON, WoodworksConfig.COMMON_SPEC);
 	}
@@ -91,9 +79,5 @@ public class Woodworks {
 		boolean includeClient = event.includeServer();
 		generator.addProvider(includeClient, new WoodworksBlockStateProvider(generator, fileHelper));
 		generator.addProvider(includeClient, new WoodworksLanguageProvider(generator));
-	}
-
-	public void registerCategories(RegisterRecipeBookCategoriesEvent event) {
-		event.registerBookCategories(Woodworks.RECIPE_TYPE_SAWING, ImmutableList.of(WoodworksRecipes.SAWING.get()));
 	}
 }
