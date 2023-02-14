38be8e18e52f1ed8eb6fc48cb279b121fdd783ed
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-02-13 19:19:41 -0600

    Fixes #19

--- src/main/java/com/teamabnormals/woodworks/common/item/crafting/SawmillRecipe.java
+++ src/main/java/com/teamabnormals/woodworks/common/item/crafting/SawmillRecipe.java
@@ -1,19 +1,19 @@
 package com.teamabnormals.woodworks.common.item.crafting;
 
 import com.teamabnormals.woodworks.core.registry.WoodworksBlocks;
+import com.teamabnormals.woodworks.core.registry.WoodworksRecipes.WoodworksRecipeSerializers;
 import com.teamabnormals.woodworks.core.registry.WoodworksRecipes.WoodworksRecipeTypes;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.SingleItemRecipe;
 import net.minecraft.world.level.Level;
 
 public class SawmillRecipe extends SingleItemRecipe {
 
 	public SawmillRecipe(ResourceLocation p_44478_, String p_44479_, Ingredient p_44480_, ItemStack p_44481_) {
-		super(WoodworksRecipeTypes.SAWING.get(), RecipeSerializer.STONECUTTER, p_44478_, p_44479_, p_44480_, p_44481_);
+		super(WoodworksRecipeTypes.SAWING.get(), WoodworksRecipeSerializers.SAWMILL.get(), p_44478_, p_44479_, p_44480_, p_44481_);
 	}
 
 	public boolean matches(Container container, Level level) {
